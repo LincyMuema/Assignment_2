59277c257be9656ae4f4e3256f8ce998c9740198
LincyMuema <lincy.muema@strathmore.edu>
2024-09-24 21:41:02 +0300

    Creation of constants for database

---
+++ connection/constants.php
@@ -0,0 +1,7 @@
+<?php
+define('DBTYPE','PDO');
+define('HOSTNAME','localhost');
+define('DBPORT','3308');
+define('HOSTUSER','root');
+define('HOSTPASS','1@3mys0lwork');
+define('DBNAME','assignment_2');
