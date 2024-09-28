cd0741ecd3afd0d1a2397af577f3541ab6d1797f
LincyMuema <lincy.muema@strathmore.edu>
2024-09-28 12:45:14 +0300

    Added color scheme to navbar

--- structure/navigation.php
+++ structure/navigation.php
@@ -7,9 +7,9 @@
             <div class="container py-4">
               <header class="pb-3 mb-4 border-bottom">
                 
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Assignment 2</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
