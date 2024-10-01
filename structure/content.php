e3e9c208ddb886c8347920cfd0931e236e0249ca
LincyMuema <lincy.muema@strathmore.edu>
2024-10-01 23:16:37 +0300

    creation of mainContent funtion

---
+++ structure/content.php
@@ -0,0 +1,17 @@
+<?php
+ class content{
+    public function mainContent(){
+        ?>
+        <div class="row align-items-md-stretch">
+        <div class="col-md-8">
+                <h1>Welcome to Assignment TWO!!!!</h1>
+                <p>
+               This is the landing page for assignment 2. Please click on sign up at the navigation bar to proceed.
+                </p>
+                <p>
+                    Have a lovely day
+    </p>
+            </div>
+        <?php
+    }
+ }
