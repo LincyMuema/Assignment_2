dbb6f6879a2323da2037545ee0de31e281ff02b3
LincyMuema <lincy.muema@strathmore.edu>
2024-10-05 18:10:07 +0300

    input validation

--- sql/insert.php
+++ sql/insert.php
@@ -1,28 +1,58 @@
-<?php
-    class insert{
-        public function signup($conn){
-            if(isset($_POST["signup"])){
-                $firstname = $_POST["firstname"];
-                $lastname = $_POST["lastname"];
-                $email = $_POST["email"];
-                $phoneNo = $_POST["phoneNo"];
-        $errors = array();
-    if(!filter_var($email_address, FILTER_VALIDATE_EMAIL)){
-        $errors['email'] = 'Invalid Email format';
-    }
 
-    $cols = ['firstname','lastname','email', 'phoneNo'];
-    $vals = [$firstname, $lastname, $email, $phoneNo];
-    
-    $data = array_combine($cols, $vals);
-    
-    $insert = $conn->insert('users', $data);
-        if($insert === TRUE){
-            header('Location: signup.php');
-            exit();
-        }else{
-            die($insert);
-        }
+<?php
+class insert {
+    public function signup($conn) {
+        $errors = []; 
+
+        if (isset($_POST["signup"])) {
+            
+            $firstname = $_POST["firstname"];
+            $lastname = $_POST["lastname"];
+            $email = $_POST["email"];
+            $phoneNo = $_POST["phoneNo"];
+
+            if (empty($firstname)) {
+                $errors['firstname'] = "First name is required.";
+            } elseif (!preg_match("/^[a-zA-Z' -]+$/", $firstname)) {
+                $errors['firstname'] = "Invalid first name. Only letters, spaces, hyphens, or apostrophes are allowed.";
+            }
+
+            if (empty($lastname)) {
+                $errors['lastname'] = "Last name is required.";
+            } elseif (!preg_match("/^[a-zA-Z' -]+$/", $lastname)) {
+                $errors['lastname'] = "Invalid last name. Only letters, spaces, hyphens, or apostrophes are allowed.";
+            }
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errors['email'] = "Invalid email format.";
+            }
+
+            if (empty($phoneNo)) {
+                $errors['phoneNo'] = "Phone number is required.";
+            } elseif (!preg_match("/^\d+$/", $phoneNo)) {
+                $errors['phoneNo'] = "Phone number must contain only numbers.";
+            }
+            if (empty($errors)) {
+                $existingUser = $conn->select('users', 'email', $email);
+                if (!empty($existingUser)) {
+                    $errors['email'] = "Email already exists.";
+                } else {
+                    $cols = ['firstname', 'lastname', 'email', 'phoneNo'];
+                    $vals = [$firstname, $lastname, $email, $phoneNo];
+                    
+                    $data = array_combine($cols, $vals);
+                    
+                    $insert = $conn->insert('users', $data);
+                    if ($insert === TRUE) {
+                        header('Location: signup.php');
+                        exit();
+                    } else {
+                        die($insert); 
+                    }
+                }
             }
         }
+
+        return $errors; 
     }
+}
