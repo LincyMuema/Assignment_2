22c57ab55938d251ebf1b034827c54c6e83d3003
LincyMuema <lincy.muema@strathmore.edu>
2024-10-06 17:43:47 +0300

    Making the inputs required

--- form/forms.php
+++ form/forms.php
@@ -2,14 +2,13 @@
 class forms {
   public function signupform($errors = []) {
       ?>
-      <form action="../after.php" method="post" enctype="multipart/form-data">
+      <form action="" method="post" enctype="multipart/form-data">
           <p>WELCOME................PLEASE SIGN UP </p>
         
-
           <div class="row mb-3">
               <label for="firstname" class="col-sm-2 col-form-label">First Name: </label>
               <div class="col-sm-10">
-                  <input type="text" name="firstname" class="form-control" id="firstname" placeholder="First Name" value="<?php echo isset($_POST['firstname']) ? htmlspecialchars($_POST['firstname']) : ''; ?>">
+                  <input type="text" name="firstname" class="form-control" id="firstname" placeholder="First Name" value="<?php echo isset($_POST['firstname']) ? htmlspecialchars($_POST['firstname']) : ''; ?>" required>
                   <?php if (isset($errors['firstname'])): ?>
                         <div class="text-danger"><?php echo $errors['firstname']; ?></div>
                     <?php endif; ?>
@@ -19,7 +18,7 @@ class forms {
           <div class="row mb-3">
               <label for="lastname" class="col-sm-2 col-form-label">Last Name: </label>
               <div class="col-sm-10">
-                  <input type="text" name="lastname" class="form-control" id="lastname" placeholder="Last Name" value="<?php echo isset($_POST['lastname']) ? htmlspecialchars($_POST['lastname']) : ''; ?>">
+                  <input type="text" name="lastname" class="form-control" id="lastname" placeholder="Last Name" value="<?php echo isset($_POST['lastname']) ? htmlspecialchars($_POST['lastname']) : ''; ?>"required>
                   <?php if (isset($errors['lastname'])): ?>
                         <div class="text-danger"><?php echo $errors['lastname']; ?></div>
                     <?php endif; ?>
@@ -29,7 +28,7 @@ class forms {
           <div class="row mb-3">
               <label for="email" class="col-sm-2 col-form-label">Email: </label>
               <div class="col-sm-10">
-                  <input type="email" name="email" class="form-control" id="email" placeholder="Email" value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : ''; ?>">
+                  <input type="email" name="email" class="form-control" id="email" placeholder="Email" value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : ''; ?>"required>
                   <?php if (isset($errors['email'])): ?>
                         <div class="text-danger"><?php echo $errors['email']; ?></div>
                     <?php endif; ?>
@@ -39,7 +38,7 @@ class forms {
           <div class="row mb-3">
               <label for="phoneNo" class="col-sm-2 col-form-label">Phone Number: </label>
               <div class="col-sm-10">
-                  <input type="text" class="form-control" id="phoneNo" name="phoneNo" placeholder="Phone Number" value="<?php echo isset($_POST['phoneNo']) ? htmlspecialchars($_POST['phoneNo']) : ''; ?>"max 13>
+                  <input type="text" class="form-control" id="phoneNo" name="phoneNo" placeholder="Phone Number" value="<?php echo isset($_POST['phoneNo']) ? htmlspecialchars($_POST['phoneNo']) : ''; ?>"minlength="10" maxlength="13" required>
                   <?php if (isset($errors['phoneNo'])): ?>
                         <div class="text-danger"><?php echo $errors['phoneNo']; ?></div>
                     <?php endif; ?>
